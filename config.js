0279c88f85292eaeb68855ca2a7f096923e79eb2
thiscold829 <107271781+thiscold829@users.noreply.github.com>
2026-05-16 20:55:19 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-  SHEET_ID: '16izs14p3VvDfojdBR2Q3Qwty6wHZiRqNbnQ8PxS9eOY',
-  API_URL: 'https://script.google.com/macros/s/AKfycbwJKhYUmbmX6txHths2kmWtHsPo5CW2asZgeJWS78qJHUJ3k5_jOYcP3MIQi1WjUE9G/exec'
+  SHEET_ID: '1NC0lrJePfSeXseeYE3j8nA-ewf_gddrcX0cDvSza18I',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwhWddKv5jjBH2C5Cjb1xp4OikM-bF0nX8NfXAnntbxIfM3u3UvUymu4ow1DjNZf4xMkA/exec'
 };
