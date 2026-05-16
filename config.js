b89864406141069f41472ef59463ef03642a1910
thiscold829 <107271781+thiscold829@users.noreply.github.com>
2026-05-16 20:53:00 +0900

    Add configuration file for API settings

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+  SHEET_ID: '16izs14p3VvDfojdBR2Q3Qwty6wHZiRqNbnQ8PxS9eOY',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwJKhYUmbmX6txHths2kmWtHsPo5CW2asZgeJWS78qJHUJ3k5_jOYcP3MIQi1WjUE9G/exec'
+};
